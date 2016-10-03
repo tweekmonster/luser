42ef2759ea2cca4bcc4929a81b9ab4e3907e503c
Tommy Allen <tommy@esdf.io>
2016-10-03 10:22:38 -0400

    Cache current user in non-cgo build.

--- current_default.go
+++ current_default.go
@@ -7,14 +7,29 @@ import (
 	"os"
 	"os/user"
 	"strconv"
+	"sync"
 )
+
+var currentUid = -1
+var current *User
+var currentMu sync.Mutex
 
 // The default stub uses $USER and $HOME to fill in the user.  A more reliable
 // method will be tried before falling back to the stub.
 func currentUser() (*User, error) {
 	uid := os.Getuid()
 	if uid >= 0 {
-		if u, err := lookupId(strconv.Itoa(uid)); err == nil {
+		if uid == currentUid && current != nil {
+			return current, nil
+		}
+
+		currentMu.Lock()
+		defer currentMu.Unlock()
+
+		currentUid = uid
+		u, err := lookupId(strconv.Itoa(uid))
+		current = u
+		if err == nil {
 			return u, nil
 		}
 	}
